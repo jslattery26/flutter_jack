b064b11a577487b8256631d298db333264a7bf04
Jack Slattery <jack@mgenio.com>
2023-02-13 11:12:14 -0500

    ;source

--- icons_and_launch_screen.sh
+++ icons_and_launch_screen.sh
@@ -1,4 +1,7 @@
 cd app
+set -a
+source .env
+set +a
 flutter pub run flutter_launcher_icons:main -f flutter_launcher_icons-dev.yaml
 flutter pub run flutter_launcher_icons:main -f flutter_launcher_icons-local.yaml
 flutter pub run flutter_launcher_icons:main -f flutter_launcher_icons-prod.yaml
